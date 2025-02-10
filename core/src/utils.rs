91d16b58f7267a92510ef21801ef0715e3b0f517
leghart <leghart@wp.pl>
2025-02-10 18:57:45 +0100

    position fixes

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,83 +1,15 @@
 use device_query::{DeviceQuery, DeviceState, MouseState};
 use image::imageops::crop_imm;
-use image::io::Reader as ImageReader;
-use image::{ColorType, ExtendedColorType, ImageBuffer, Luma, Rgba};
+use image::{ColorType, ImageBuffer, Luma, Rgba};
 use screenshots::Screen;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::{thread, time::Duration};
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 pub enum Color {
     WHITE,
     BLACK,
-}
-
-pub struct BoardPieces {
-    pawns: (
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-    ),
-    rooks: (
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-    ),
-    bishops: (
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-    ),
-    knights: (
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-    ),
-    queens: (
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-    ),
-    kings: (
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-        ImageBuffer<Luma<u8>, Vec<u8>>,
-    ),
-}
-
-impl BoardPieces {
-    pub fn default(directory: &str) -> Result<Self, String> {
-        Ok(Self {
-            pawns: (
-                BoardPieces::load_piece(directory, "pawn_white.png")?,
-                BoardPieces::load_piece(directory, "pawn_black.png")?,
-            ),
-            rooks: (
-                BoardPieces::load_piece(directory, "rook_white.png")?,
-                BoardPieces::load_piece(directory, "rook_black.png")?,
-            ),
-            bishops: (
-                BoardPieces::load_piece(directory, "bishop_white.png")?,
-                BoardPieces::load_piece(directory, "bishop_black.png")?,
-            ),
-            knights: (
-                BoardPieces::load_piece(directory, "knight_white.png")?,
-                BoardPieces::load_piece(directory, "knight_black.png")?,
-            ),
-            queens: (
-                BoardPieces::load_piece(directory, "queen_white.png")?,
-                BoardPieces::load_piece(directory, "queen_black.png")?,
-            ),
-            kings: (
-                BoardPieces::load_piece(directory, "king_white.png")?,
-                BoardPieces::load_piece(directory, "king_black.png")?,
-            ),
-        })
-    }
-
-    fn load_piece(directory: &str, image: &str) -> Result<ImageBuffer<Luma<u8>, Vec<u8>>, String> {
-        let path = Path::new(directory).join(image);
-        let img = ImageReader::open(&path)
-            .map_err(|_| format!("Not found {:?}", path))?
-            .decode()
-            .map_err(|_| format!("Decode error {:?}", path))?;
-        Ok(img.to_luma8())
-    }
 }
 
 #[derive(Debug, Default)]
@@ -174,15 +106,37 @@ pub fn get_screen_area() -> Result<ScreenArea, Box<dyn std::error::Error>> {
     Ok(final_selection)
 }
 
-pub fn extract_board_state(image_data: (Vec<u8>, u32, u32)) -> [[bool; 8]; 8] {
-    let _vec = image_data.0;
-    let width = image_data.1;
-    let height = image_data.2;
+pub fn extract_board_state(
+    image: ImageBuffer<Luma<u8>, Vec<u8>>,
+) -> Result<[[Option<Color>; 8]; 8], &'static str> {
+    let width = image.width();
+    let height = image.height();
+    let _vec = image.to_vec();
 
     let square_width = width / 8;
     let square_height = height / 8;
 
-    let mut board = [[false; 8]; 8];
+    let mut board: [[Option<Color>; 8]; 8] = [[None; 8]; 8];
+
+    for row in 1..8 {
+        let y = row * square_height;
+        for x in 0..width {
+            let pixel_index = (y * width + x) as usize;
+            if _vec[pixel_index] == 0 {
+                return Err("Detected a piece between squares on a horizontal grid line!");
+            }
+        }
+    }
+
+    for col in 1..8 {
+        let x = col * square_width;
+        for y in 0..height {
+            let pixel_index = (y * width + x) as usize;
+            if _vec[pixel_index] == 0 {
+                return Err("Detected a piece between squares on a vertical grid line!");
+            }
+        }
+    }
 
     for row in 0..8 {
         for col in 0..8 {
@@ -199,28 +153,52 @@ pub fn extract_board_state(image_data: (Vec<u8>, u32, u32)) -> [[bool; 8]; 8] {
                 }
             }
 
-            board[row as usize][col as usize] =
-                (black_pixel_count * 100 / all) as f32 > 0.05 as f32;
+            // TODO: impl for white
+            if (black_pixel_count * 100 / all) as f32 > 0.05 as f32 {
+                // TODO!
+                if black_pixel_count > 1800 {
+                    board[row as usize][col as usize] = Some(Color::BLACK);
+                } else {
+                    board[row as usize][col as usize] = Some(Color::WHITE);
+                }
+            }
         }
     }
 
-    board
+    Ok(board)
 }
 
 pub fn detect_move(
-    before: &[[bool; 8]; 8],
-    after: &[[bool; 8]; 8],
+    before: &[[Option<Color>; 8]; 8],
+    after: &[[Option<Color>; 8]; 8],
 ) -> ((usize, usize), (usize, usize)) {
     let mut start_pos = None;
     let mut end_pos = None;
 
+    println!("before: {:?} after: {:?}", before, after);
+
+    //TODO!: exf4 panic
+
     for row in 0..8 {
         for col in 0..8 {
-            if before[row][col] && !after[row][col] {
-                start_pos = Some((col, row));
+            // moved to a new (empty) place
+            if start_pos.is_none() && before[row][col].is_some() && after[row][col].is_none() {
+                start_pos = Some((col, row)); // TODO: handle en passant
+                continue;
             }
-            if !before[row][col] && after[row][col] {
+
+            // piece took another piece
+            if end_pos.is_none() && before[row][col].is_some() && after[col][row].is_some() {
+                if before[row][col] != after[row][col] {
+                    println!("END 1 {col} {row}");
+                    end_pos = Some((col, row));
+                }
+            } else if end_pos.is_none() && before[row][col].is_none() && after[row][col].is_some() {
                 end_pos = Some((col, row));
+            }
+
+            if end_pos.is_some() && start_pos.is_some() {
+                return (start_pos.unwrap(), end_pos.unwrap());
             }
         }
     }
@@ -231,165 +209,67 @@ pub fn detect_move(
     )
 }
 
-pub fn pixel_to_chess_coord(x: usize, y: usize, board_width: usize, board_height: usize) -> String {
-    let file = (b'a' + x as u8) as char; //  (a-h)
-    let rank = (board_height - y).to_string(); //  (1-8)
-    format!("{}{}", file, rank)
+pub fn pixel_to_chess_coord(x: usize, y: usize) -> String {
+    let col = (b'a' + x as u8) as char; //  (a-h)
+    let row = 8 - y; // (1-8)
+    format!("{}{}", col, row)
 }
 
-pub fn check_if_board_was_changed(previous: &[[bool; 8]; 8], current: &[[bool; 8]; 8]) -> bool {
-    // TODO: temporary check if two positions were changed -> potential problem with promote pawns
-    let mut counter = 0;
+pub fn check_if_board_was_changed(
+    previous: &[[Option<Color>; 8]; 8],
+    current: &[[Option<Color>; 8]; 8],
+) -> bool {
     for row in 0..8 {
         for col in 0..8 {
             if previous[row][col] != current[row][col] {
-                // return true;
-                counter += 1;
+                return true;
             }
         }
     }
 
-    return counter == 2;
+    return false;
 }
 
 pub fn runner() {
     println!("Take a screenshot");
     let selection = get_screen_area().unwrap();
-    let i = take_screenshot(&selection).unwrap();
-    let image = to_binary(&i, 70);
-    let width = image.width();
-    let height = image.height();
-    let data = image.clone().into_vec();
-    let board: [[bool; 8]; 8] = extract_board_state((data, width, height));
-    let mut previous_board = board.clone();
+    let ref_image: ImageBuffer<Luma<u8>, Vec<u8>> = {
+        let raw_image = take_screenshot(&selection).unwrap();
+        let binary_image = to_binary(&raw_image, 70);
+        trimm(&binary_image)
+    };
 
-    for _ in 0..500 {
-        std::thread::sleep(std::time::Duration::from_millis(500));
-        let sss = std::time::Instant::now();
-        let ni = take_screenshot(&selection).unwrap();
-        let nimage = to_binary(&ni, 70);
-        nimage.save(std::path::Path::new("dupa.png")).unwrap();
-        let raw_data = nimage.clone().into_vec();
-        let current = extract_board_state((raw_data.clone(), width, height));
+    let mut previous_board = extract_board_state(ref_image).unwrap();
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+        let start_time = std::time::Instant::now();
+
+        let new_image = {
+            let raw_image = take_screenshot(&selection).unwrap();
+            let bin = to_binary(&raw_image, 70);
+            trimm(&bin)
+            // TODO: change trim to updated selecion area with trimmed dimensions
+        };
+        new_image.save(std::path::Path::new("updated.png")).unwrap(); // tmp
+
+        let current_result = extract_board_state(new_image); //TODO: missing info about piece color
+
+        if current_result.is_err() {
+            continue;
+        }
+
+        let current = current_result.unwrap();
+
         if check_if_board_was_changed(&previous_board, &current) {
-            test_save_img(&raw_data, width as u32, height as u32);
             let (start, end) = detect_move(&previous_board, &current);
-            let start_pos = pixel_to_chess_coord(start.0, start.1, width as usize, height as usize);
-            let end_pos = pixel_to_chess_coord(end.0, end.1, width as usize, height as usize);
-            println!("{} -> {}", start_pos, end_pos);
-            println!("{:?}", sss.elapsed());
+            let start_pos = pixel_to_chess_coord(start.0, start.1);
+            let end_pos = pixel_to_chess_coord(end.0, end.1);
+            println!("{} -> {} [{:?}]", start_pos, end_pos, start_time.elapsed());
             previous_board = current;
-        } else {
-            println!("Not found any changes")
         }
     }
-}
-
-// pub fn runner() {
-//     println!("Take a screenshot");
-//     let selection = get_screen_area().unwrap();
-//     let initial = take_screenshot(&selection).unwrap();
-//     let width = initial.1 as usize;
-//     let height = initial.2 as usize;
-//     let board: [[bool; 8]; 8] = extract_board_state(initial);
-//     let mut previous_board = board.clone();
-
-//     for _ in 0..50 {
-//         std::thread::sleep(std::time::Duration::from_secs(1));
-//         let ndata: (Vec<u8>, u32, u32) = take_screenshot(&selection).unwrap();
-//         let raw_data = ndata.clone().0;
-//         let current = extract_board_state(ndata);
-//         if check_if_board_was_changed(&previous_board, &current) {
-//             test_save_img(&raw_data, width as u32, height as u32);
-//             let (start, end) = detect_move(&previous_board, &current);
-//             let start_pos = pixel_to_chess_coord(start.0, start.1, width, height);
-//             let end_pos = pixel_to_chess_coord(end.0, end.1, width, height);
-//             println!("{} -> {}", start_pos, end_pos);
-//             // println!("any changes{}")
-//             previous_board = current;
-//         } else {
-//             println!("Not found any changes")
-//         }
-//     }
-// }
-
-pub fn calculate_rms(img1: &(Vec<u8>, u32, u32), img2: &(Vec<u8>, u32, u32)) -> f64 {
-    let (data1, w1, h1) = img1;
-    let (data2, w2, h2) = img2;
-
-    if w1 != w2 || h1 != h2 || data1.len() != data2.len() {
-        return f64::MAX;
-    }
-
-    let sum_diff: f64 = data1
-        .iter()
-        .zip(data2.iter())
-        .map(|(p1, p2)| (*p1 as f64 - *p2 as f64).powi(2))
-        .sum();
-
-    (sum_diff / data1.len() as f64).sqrt()
-}
-
-pub fn player_color(image: &ImageBuffer<Luma<u8>, Vec<u8>>) -> Color {
-    // TODO!
-    Color::WHITE
-}
-
-pub fn create_piece_templates(
-    image: &ImageBuffer<Luma<u8>, Vec<u8>>,
-    color: Color,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let (width, height) = image.dimensions();
-    let square_width = width / 8;
-    let square_height = height / 8;
-
-    let map: std::collections::HashMap<&str, (usize, usize)>;
-    if color == Color::WHITE {
-        map = std::collections::HashMap::from([
-            ("rook_black", (0, 0)),
-            ("rook_white", (7, 0)),
-            ("knight_black", (0, 1)),
-            ("knight_white", (7, 1)),
-            ("bishop_black", (0, 2)),
-            ("bishop_white", (7, 2)),
-            ("queen_black", (0, 3)),
-            ("queen_white", (7, 3)),
-            ("king_black", (0, 4)),
-            ("king_white", (7, 4)),
-            ("pawn_black", (6, 0)),
-            ("pawn_white", (1, 0)),
-        ]);
-    } else {
-        map = std::collections::HashMap::from([
-            ("rook_white", (0, 0)),
-            ("rook_black", (7, 0)),
-            ("knight_white", (0, 1)),
-            ("knight_black", (7, 1)),
-            ("bishop_white", (0, 2)),
-            ("bishop_black", (7, 2)),
-            ("king_white", (0, 3)),
-            ("king_black", (7, 3)),
-            ("queen_white", (0, 4)),
-            ("queen_black", (7, 4)),
-            ("pawn_white", (6, 0)),
-            ("pawn_black", (1, 0)),
-        ]);
-    }
-
-    for (piece, (row, col)) in map {
-        let x = (col as u32) * square_width;
-        let y = (row as u32) * square_height;
-
-        let square = crop_imm(image, x, y, square_width, square_height).to_image();
-
-        let filename = format!("templates/piece_{}.png", piece);
-        square.save(filename).unwrap();
-    }
-    Ok(())
-}
-
-pub fn load_pieces_from_templates() -> BoardPieces {
-    BoardPieces::default("templates").expect("Can not load templates")
+    println!("STOP");
 }
 
 pub fn take_screenshot(
