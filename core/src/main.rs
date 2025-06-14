7d9b698635a658e65a0cb47bc6e54736f7228e6b
leghart <leghart@wp.pl>
2025-06-14 13:29:58 +0000

    auto detect board coords and auto trim

--- core/src/main.rs
+++ core/src/main.rs
@@ -32,61 +32,97 @@ use image::{imageops, ImageBuffer, Rgba};
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
-fn main() {
-    let (tx, rx): (Sender<Mat>, Receiver<Mat>) = bounded(10);
-    let prev_hash = Arc::new(Mutex::new(0u64));
+use opencv::{prelude::*, types, Result};
 
-    let prev_hash_clone = Arc::clone(&prev_hash);
-    thread::spawn(move || {
-        let monitor = Monitor::from_point(100, 100).unwrap();
-        loop {
-            let st = Instant::now();
-            if let Ok(capture) = monitor.capture_image() {
-                let raw = ImageBuffer::<Rgba<u8>, _>::from_raw(
-                    capture.width(),
-                    capture.height(),
-                    capture.into_vec(),
-                )
-                .unwrap();
+fn select_primary_monitor(primary: bool) -> Option<Monitor> {
+    for m in Monitor::all().unwrap() {
+        if primary && m.is_primary().unwrap() {
+            return Some(m);
+        } else if !primary && !m.is_primary().unwrap() {
+            return Some(m);
+        }
+    }
+    None
+}
 
-                let cropped = imageops::crop_imm(&raw, 443, 183, 747, 745).to_image();
-                let dynimage = DynamicImage::ImageRgba8(cropped);
+fn capture_entire_screen(monitor: &Monitor) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let capture = monitor.capture_image().unwrap();
 
-                let hash = image_hash(&dynimage);
-                let mut last_hash = prev_hash_clone.lock().unwrap();
-                if *last_hash != hash {
-                    *last_hash = hash;
-                    if let Ok(mat) = dynamic_image_to_mat(&dynimage) {
-                        let resized = myimage::ImageProcessing::resize(&mat, 360, 360).unwrap();
-                        // println!("send");
-                        tx.send(resized).ok();
-                    }
-                }
-                // print!("{:?}", st.elapsed());
+    ImageBuffer::<Rgba<u8>, _>::from_raw(capture.width(), capture.height(), capture.into_vec())
+        .unwrap()
+}
+
+// This function captures the screen and returns the region of the chessboard
+// with the following steps:
+// - Convert the captured image to grayscale
+// - Apply Canny edge detection to find the edges
+// - Find contours in the edge-detected image
+// - Approximate the contours to find quadrilaterals
+fn get_board_region(raw: &Mat) -> (u32, u32, u32, u32) {
+    let mut gray = Mat::default();
+    imgproc::cvt_color(&raw, &mut gray, imgproc::COLOR_BGR2GRAY, 0).unwrap();
+
+    // contours without blurring to keep sharp edges
+    let mut edges = Mat::default();
+    imgproc::canny(&gray, &mut edges, 50.0, 150.0, 3, false).unwrap();
+
+    let mut contours = opencv::core::Vector::<opencv::core::Vector<Point>>::new();
+    imgproc::find_contours(
+        &edges,
+        &mut contours,
+        imgproc::RETR_EXTERNAL,
+        imgproc::CHAIN_APPROX_SIMPLE,
+        Point::new(0, 0),
+    )
+    .unwrap();
+
+    let mut max_area = 0.0;
+    let mut best_quad = vec![];
+
+    for contour in contours {
+        let mut approx = opencv::core::Vector::<Point>::new();
+        imgproc::approx_poly_dp(
+            &contour,
+            &mut approx,
+            0.02 * imgproc::arc_length(&contour, true).unwrap(),
+            true,
+        )
+        .unwrap();
+
+        if approx.len() == 4 && imgproc::is_contour_convex(&approx).unwrap() {
+            let area = imgproc::contour_area(&approx, false).unwrap();
+            let bounding = imgproc::bounding_rect(&approx).unwrap();
+
+            let aspect_ratio = bounding.width as f32 / bounding.height as f32;
+            if area > max_area && aspect_ratio > 0.8 && aspect_ratio < 1.2 {
+                max_area = area;
+                best_quad = approx.to_vec();
             }
         }
-    });
-
-    let tracker = ChesscomWrapper::default();
-
-    let pieces = tracker.load_pieces().unwrap();
-
-    while let Ok(mat) = rx.recv() {
-        // println!("recv");
-        let st = Instant::now();
-        let board_data = tracker.process_image(&mat, &pieces).unwrap();
-        // println!("process: {:?}", st.elapsed());
-
-        let board = engine::Board::new(board_data);
-        board.print();
-        println!("{:?}", st.elapsed());
-        // myimage::ImageProcessing::show(&mat, false).unwrap();
-        // break;
-
-        // if let Ok(board_data) = process_image(&mat) {
-        //     println!("Got board data: {:?}", board_data);
-        // }
     }
+
+    let x_start = best_quad[0].x as u32;
+    let y_start = best_quad[0].y as u32;
+    let width = best_quad[2].x as u32 - x_start;
+    let height = best_quad[2].y as u32 - y_start;
+
+    (x_start, y_start, width, height)
+}
+
+fn main() {
+    let monitor = select_primary_monitor(false);
+
+    let monitor = monitor.expect("No primary monitor found");
+    let raw = capture_entire_screen(&monitor);
+    let dyn_image = DynamicImage::ImageRgba8(raw.clone());
+
+    let rat = dynamic_image_to_mat(&dyn_image).unwrap();
+    let coords = get_board_region(&rat);
+
+    let cropped = imageops::crop_imm(&raw, coords.0, coords.1, coords.2, coords.3).to_image();
+    let dynimage = DynamicImage::ImageRgba8(cropped);
+    let board = dynamic_image_to_mat(&dynimage).unwrap();
+    myimage::ImageProcessing::show(&board, true).unwrap();
 }
 
 fn dynamic_image_to_mat(img: &DynamicImage) -> opencv::Result<Mat> {
@@ -94,82 +130,18 @@ fn dynamic_image_to_mat(img: &DynamicImage) -> opencv::Result<Mat> {
     let (width, height) = rgba8.dimensions();
 
     let mut mat =
-        Mat::new_rows_cols_with_default(height as i32, width as i32, CV_8UC4, Scalar::all(0.0))?;
+        Mat::new_rows_cols_with_default(height as i32, width as i32, CV_8UC4, Scalar::all(0.0))
+            .unwrap();
 
-    let mat_data = mat.data_bytes_mut()?;
+    let mat_data = mat.data_bytes_mut().unwrap();
     mat_data.copy_from_slice(&rgba8.as_raw());
 
     let mut mat_bgr = Mat::default();
-    imgproc::cvt_color(&mat, &mut mat_bgr, imgproc::COLOR_RGBA2BGR, 0)?;
+    imgproc::cvt_color(&mat, &mut mat_bgr, imgproc::COLOR_RGBA2BGR, 0).unwrap();
 
     Ok(mat_bgr)
-}
-
-fn image_hash(img: &DynamicImage) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    img.to_rgba8().as_raw().hash(&mut hasher);
-    hasher.finish()
 }
 
 fn process_image(image: &Mat) -> Result<[[char; 8]; 8], Box<dyn std::error::Error>> {
     Ok([['.'; 8]; 8])
 }
-
-// fn _single_run() {
-//     let total = std::time::Instant::now();
-//     let st = std::time::Instant::now();
-//     let tracker = ChesscomWrapper::default();
-//     println!("tracker: {:?}", st.elapsed());
-
-//     let st = std::time::Instant::now();
-//     let image = tracker.capture_screenshot().unwrap();
-//     println!("screenshot: {:?}", st.elapsed());
-
-//     let st = std::time::Instant::now();
-//     let resized = ImageProcessing::resize(&image, 360, 360).unwrap();
-//     println!("resize: {:?}", st.elapsed());
-
-//     let st = std::time::Instant::now();
-//     let pieces = tracker.load_pieces().unwrap();
-//     println!("laod pieces: {:?}", st.elapsed());
-
-//     let st = std::time::Instant::now();
-//     let _ = tracker.process_image(&resized, &pieces).unwrap();
-
-//     println!("process: {:?}", st.elapsed());
-//     println!("TOTOAL: {:?}", total.elapsed());
-
-//     println!("\x1b[33m♕\x1b[0m");
-// }
-
-// fn _loop() {
-//     let tracker_def = ChesscomWrapper::default();
-//     // 440 219 758 759
-//     // 445, 185, 742, 743
-//     let tracker = ChesscomWrapper::new(
-//         utils::screen_region::ScreenRegion::new(443, 183, 744, 745),
-//         tracker_def.get_thresholds().clone(),
-//     );
-//     println!("tracker region: {:?}", tracker.get_region());
-//     let pieces = tracker.load_pieces().unwrap();
-
-//     loop {
-//         let st = std::time::Instant::now();
-//         // println!("start: {:?}", st.elapsed());
-
-//         let image = tracker.capture_screenshot().unwrap();
-//         // println!("screenshot: {:?}", st.elapsed());
-
-//         let resized = ImageProcessing::resize(&image, 360, 360).unwrap();
-//         // println!("resize: {:?}", st.elapsed());
-
-//         let board_data = tracker.process_image(&resized, &pieces).unwrap();
-//         // println!("process: {:?}", st.elapsed());
-
-//         let board = engine::Board::new(board_data);
-//         board.print();
-//         // println!("{:?}", file_name);
-//         // println!("cycle: {:?}", st.elapsed());
-//         std::thread::sleep(std::time::Duration::from_millis(200));
-//     }
-// }
