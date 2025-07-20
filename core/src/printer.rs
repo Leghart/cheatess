cbdecfe51a5425a066b1c09f0c8b1f6d40e657c3
Dawid Sielużycki <48066889+Leghart@users.noreply.github.com>
2025-07-20 09:56:07 +0000

    [fix]: replace f32 to String in get_evaluation (#48)

---
+++ core/src/printer.rs
@@ -0,0 +1,75 @@
+use std::io::Write;
+
+static PIECE_TABLE: [&str; 128] = {
+    let mut table = [""; 128];
+    table['r' as usize] = "♜";
+    table['n' as usize] = "♞";
+    table['b' as usize] = "♝";
+    table['q' as usize] = "♛";
+    table['k' as usize] = "♚";
+    table['p' as usize] = "♟";
+    table['R' as usize] = "♖";
+    table['N' as usize] = "♘";
+    table['B' as usize] = "♗";
+    table['Q' as usize] = "♕";
+    table['K' as usize] = "♔";
+    table['P' as usize] = "♙";
+    table
+};
+
+fn get_piece(c: char) -> Option<&'static str> {
+    if (c as usize) < 128 {
+        Some(PIECE_TABLE[c as usize])
+    } else {
+        None
+    }
+}
+
+pub trait Printer {
+    fn print_piece(piece: char) -> String;
+}
+
+pub struct DefaultPrinter;
+
+impl Printer for DefaultPrinter {
+    fn print_piece(piece: char) -> String {
+        piece.to_string()
+    }
+}
+
+pub struct PrettyPrinter;
+impl Printer for PrettyPrinter {
+    fn print_piece(piece: char) -> String {
+        get_piece(piece).unwrap_or(" ").to_string()
+    }
+}
+
+pub trait AnyBoard<P: Printer> {
+    fn print(&self, writer: &mut dyn Write);
+    fn raw(&self) -> &[[char; 8]; 8];
+}
+
+pub trait View {
+    fn row(i: usize) -> usize;
+    fn col(i: usize) -> usize;
+}
+
+pub struct WhiteView;
+impl View for WhiteView {
+    fn row(i: usize) -> usize {
+        i
+    }
+    fn col(i: usize) -> usize {
+        i
+    }
+}
+
+pub struct BlackView;
+impl View for BlackView {
+    fn row(i: usize) -> usize {
+        7 - i
+    }
+    fn col(i: usize) -> usize {
+        7 - i
+    }
+}
