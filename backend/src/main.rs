c8f9763b2f312a4b1824a482ce1a4c35ddb3813a
leghart <leghart@wp.pl>
2025-08-01 06:18:15 +0000

    [refactor]: change for single repo

--- backend/src/main.rs
+++
@@ -1,7 +0,0 @@
-use cheatess_core::core::engine::{create_board_default, Color, DefaultPrinter};
-use std::io;
-
-fn main() {
-    let board = create_board_default::<DefaultPrinter>(&Color::Black);
-    board.print(&mut io::stdout());
-}
